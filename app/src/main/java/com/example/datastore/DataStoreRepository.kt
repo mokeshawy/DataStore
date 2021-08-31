441bd9ad52c2067bf760ad23bc908ae03fd10853
mohamed <mohamed_gsm@msn.com>
2021-08-31 12:58:03 +0200

    create dataStore repository

---
+++ app/src/main/java/com/example/datastore/DataStoreRepository.kt
@@ -0,0 +1,36 @@
+package com.example.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
+
+class DataStoreRepository(private val context: Context) {
+
+    private val Context.dataStore : DataStore<Preferences> by preferencesDataStore("pref")
+
+    companion object{
+        val USERNAME = stringPreferencesKey("USER_NAME")
+        val AGE = intPreferencesKey("AGE")
+    }
+
+    suspend fun storeUser( name : String , age : Int){
+        context.dataStore.edit {
+            it[USERNAME]    = name
+            it[AGE]         = age
+        }
+    }
+
+    fun getUserName() = context.dataStore.data.map {
+        it[USERNAME] ?:""
+    }
+
+    fun getUserAge() = context.dataStore.data.map {
+        it[AGE] ?: -1
+    }
+
+}
